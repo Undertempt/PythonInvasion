664d4ef5f431e8fce81d0b3d3b93a03e24117207
Gavin Lavergne <gavinlavergne12@gmail.com>
2026-02-24 23:41:10 -0600

    Added settings, images folder, and snake class

--- python_invasion.py
+++ python_invasion.py
@@ -1,16 +1,22 @@
 import sys
 import pygame
-
+from settings import Settings
+from snake import Snake
 class PythonInvasion:
-
+    """Overall class to manage game assets and behavior"""
     def __init__(self):
-        """Considered to be the on switch for the library. Initializes background settings needed to work properly"""
+        """Initialize the game and create game resources"""
+        #Considered to be the on switch for the library. Initializes background settings needed to work properly
         pygame.init
         #Clock used to ensure game runs at same speed on all machines
         self.clock = pygame.time.Clock()
+        self.settings = Settings()
         #Creates a display window that is 1200 pixels wide and 800 pixels tall
-        self.screen = pygame.display.set_mode((1200,800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Python Invasion")
+        self.snake = Snake(self)
+        #Set BG color
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Starts the main loop for the game"""
@@ -18,7 +24,10 @@ class PythonInvasion:
             # Watch for keyboard and mouse events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    sys.exit()          
+                    sys.exit() 
+            #Fills screen with the BG color    
+            self.screen.fill(self.settings.bg_color)  
+            self.snake.blitme()
             #Makes the most recently drawn screen visible
             pygame.display.flip()
             #Tells pygame to ensure the loop runs 60 times per second
@@ -26,7 +35,7 @@ class PythonInvasion:
 
 
 if __name__ == '__main__':
-    #Makes a game instance and runs the game'
+    """Makes a game instance and runs the game"""
     pi = PythonInvasion()
     pi.run_game()
 
